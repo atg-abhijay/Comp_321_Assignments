e55568749f9d05e329ecc5fcbbb977b182784d19
atg-abhijay <atg.abhijay@gmail.com>
2018-09-19 11:27:23 -0400

    making a list out of the given number

--- Assignment_1/Yoda.java
+++ Assignment_1/Yoda.java
@@ -1,14 +1,20 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+
 
 public class Yoda {
     public static void main(String[] args) {
         List<Integer> inputList = read();
-        for (Integer num: inputList) {
-            System.out.println(num);
-        }
+        // for (Integer num: inputList) {
+        //     System.out.println(num);
+        // }
+        List<Integer> l1 = makeList(inputList.get(0));
+        List<Integer> l2 = makeList(inputList.get(1));
+        System.out.println(l1 + "\n" + l2);
     }
+
 
     public static List<Integer> read() {
         List<Integer> inputList = new ArrayList<Integer>();
@@ -19,5 +25,17 @@ public class Yoda {
         sc.close();
 
         return inputList;
+    }
+
+
+    public static List<Integer> makeList(int num) {
+        List<Integer> numAsList = new ArrayList<Integer>();
+        while (num > 0) {
+            int digit = num % 10;
+            numAsList.add(digit);
+            num = num / 10;
+        }
+        Collections.reverse(numAsList);
+        return numAsList;
     }
 }
