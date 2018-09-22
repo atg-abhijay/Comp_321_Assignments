3d04d52cd0b48997e27a44f5b7aa6cfc44618abb
atg-abhijay <atg.abhijay@gmail.com>
2018-09-22 19:31:09 -0400

    renamed file and added comments

--- Assignment_1/EasiestProblem_260708548.java
+++ Assignment_1/EasiestProblem_260708548.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Easiest {
+public class EasiestProblem_260708548 {
     public static void main(String[] args) {
         List<Integer> inputList = readInput();
         for (int inputNum : inputList) {
@@ -11,17 +11,21 @@ public class Easiest {
             boolean foundP = false;
             int p = 10;
             /**
-             * this will form the base number
-             * and we will add the input number
-             * to it at each iteration. increment
-             * 'p' at each iteration.
+             * Under the premise that addition is faster than
+             * multiplication, instead of having to calculate inputNum*p
+             * repeatedly, form the base number
+             * (product) as inputNum*10 and add inputNum
+             * to it at each iteration. increment p at
+             * each iteration as well. Adding just the number
+             * at each iteration is much simpler than having
+             * to multiply with a brand new number.
              */
             int product = inputNum * p;
             while (!foundP) {
                 product += inputNum;
                 p += 1;
-                int productSumOfDig = calculateSum(product);
-                if (productSumOfDig == inputNumSum) {
+                int productSumOfDigits = calculateSum(product);
+                if (productSumOfDigits == inputNumSum) {
                     foundP = true;
                     System.out.println(p);
                 }
