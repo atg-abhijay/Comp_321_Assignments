40af6800d65192f70bc73b9fc3fb3d46a3ef08ab
atg-abhijay <atg.abhijay@gmail.com>
2018-09-22 22:54:58 -0400

    finished writing code for it. have to test against input

--- Assignment_1/GuessDataStruc_260708548.java
+++ Assignment_1/GuessDataStruc_260708548.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -7,9 +8,67 @@ public class GuessDataStruc_260708548 {
     public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> rowsOfTestCases = readInput();
         for (ArrayList<Integer> tcRow : rowsOfTestCases) {
-            break;
+
+            int[] points = new int[3];
+            String result = "";
+            List<Integer> bagOfNumbers = new ArrayList<Integer>();
+            for (int num : tcRow) {
+                if (num > 0) {
+                    bagOfNumbers.add(num);
+                }
+                else {
+                    int outcome = evalBagAfterRemoval(bagOfNumbers, num * (-1), points);
+                    if (outcome == -1) {
+                        result = "impossible";
+                        break;
+                    }
+                }
+            }
+
+            if (result != "impossible") {
+                int stackPoints = points[0];
+                int queuePoints = points[1];
+                int priorityQueuePoints = points[2];
+                boolean isStack = stackPoints != 0 && queuePoints == 0 && priorityQueuePoints == 0;
+                boolean isQueue = queuePoints != 0 && stackPoints == 0 && priorityQueuePoints == 0;
+                boolean isPriorityQueue = priorityQueuePoints != 0 && stackPoints == 0 && queuePoints == 0;
+
+                if (isStack) {
+                    result = "stack";
+                }
+                else if (isQueue) {
+                    result = "queue";
+                }
+                else if (isPriorityQueue) {
+                    result = "priority queue";
+                }
+                else if ((isStack && isQueue) || (isStack && isPriorityQueue) || (isQueue &&isPriorityQueue)) {
+                    result = "not sure";
+                }
+            }
+
+            System.out.println(result);
         }
 
+    }
+
+
+    public static int evalBagAfterRemoval(List<Integer> bagOfNumbers, int num, int[] points) {
+        if (!bagOfNumbers.contains(num)) {
+            return -1;
+        }
+        else {
+            if (num == bagOfNumbers.get(0)) {
+                points[1] += 1;
+            }
+            if (num == bagOfNumbers.get(bagOfNumbers.size()-1)) {
+                points[0] += 1;
+            }
+            if (num == Collections.max(bagOfNumbers)) {
+                points[2] += 1;
+            }
+            return 0;
+        }
     }
 
 
