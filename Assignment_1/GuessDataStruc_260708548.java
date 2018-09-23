dc7ead1deb0ec90c8abf6a579e37ef112ed60776
atg-abhijay <atg.abhijay@gmail.com>
2018-09-22 21:19:28 -0400

    made readInput() method

--- Assignment_1/GuessDataStruc_260708548.java
+++ Assignment_1/GuessDataStruc_260708548.java
@@ -1,5 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class GuessDataStruc_260708548 {
     public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> rowsOfTestCases = readInput();
+        for (ArrayList<Integer> tcRow : rowsOfTestCases) {
+            break;
+        }
 
     }
+
+
+    public static ArrayList<ArrayList<Integer>> readInput() {
+        ArrayList<ArrayList<Integer>> rowsOfTestCases = new ArrayList<ArrayList<Integer>>();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int numLines = sc.nextInt();
+            ArrayList<Integer> tcRow = new ArrayList<Integer>();
+            for (int i = 0; i < numLines; i++) {
+                int command = sc.nextInt();
+                if (command == 1) {
+                    tcRow.add(sc.nextInt());
+                }
+                else {
+                    tcRow.add(sc.nextInt() * -1);
+                }
+            }
+            rowsOfTestCases.add(tcRow);
+        }
+
+        sc.close();
+        return rowsOfTestCases;
+    }
+
+
+    // public static void test() {
+    //     Scanner sc = new Scanner(System.in);
+    //     int[] nums = new int[2];
+    //     for(int i = 0; i < 2; i++) {
+    //         int num = sc.nextInt();
+    //         nums[i] = num;
+    //     }
+    //     for (int num : nums) {
+    //         System.out.println(num);
+    //     }
+    // }
 }
