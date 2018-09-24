7ae10b0450e16f2cdb2b0040403127022c76bf9a
atg-abhijay <atg.abhijay@gmail.com>
2018-09-24 15:33:59 -0400

    A2 - initial code commit

---
+++ Assignment_2/DisastDowntime_260708548.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DisastDowntime_260708548 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> servers = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> server = new ArrayList<>();
+        int upcomingRequests = sc.nextInt();
+        int serverRequestsPerSecond = sc.nextInt();
+        while (sc.hasNextInt()) {
+
+        }
+    }
+}
