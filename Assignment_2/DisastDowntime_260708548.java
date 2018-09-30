f8dfbea31d36f32ab3d8eedd04be56c408a9a203
atg-abhijay <atg.abhijay@gmail.com>
2018-09-30 12:25:11 -0400

    trying to use hashmpa

--- Assignment_2/DisastDowntime_260708548.java
+++ Assignment_2/DisastDowntime_260708548.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class DisastDowntime_260708548 {
@@ -6,17 +9,26 @@ public class DisastDowntime_260708548 {
         Scanner sc = new Scanner(System.in);
         int upcomingRequests = sc.nextInt();
         int serverRequestsPerSecond = sc.nextInt();
-        ArrayList<ArrayList<Integer>> serversList = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> server = new ArrayList<>();
-        for(int i = 0; i < serversList.size(); i++) {
-            serversList.add(new ArrayList<Integer>(serverRequestsPerSecond));
-        }
+        HashMap servers = new HashMap<Integer, List<Integer>>();
+        int num_servers = servers.size();
+        List<Integer> serv = new ArrayList<Integer>();
+        servers.put(0, serv);
+        // System.out.println(servers.get(0));
+        // List<List<Integer>> serversList = new ArrayList<List<Integer>>();
+        // List<Integer> server = new ArrayList<>();
+        // for(int i = 0; i < serversList.size(); i++) {
+        //     /**
+        //      * more time efficient to initialize the size of
+        //      * the arraylists than to not specify the size
+        //      */
+        //     serversList.add(new ArrayList<Integer>(serverRequestsPerSecond));
+        // }
         for(int i = 0; i < upcomingRequests; i++) {
             boolean addedRequest = false;
             int newRequest = sc.nextInt();
             while (!addedRequest) {
-                for (int j = 0; j < serversList.size(); j++) {
-                    server = serversList.get(j);
+                for (int j = 0; j < servers.size(); j++) {
+                    server = servers.get(j);
                     updateServer(server, newRequest);
                     if (server.size() != serverRequestsPerSecond) {
                         server.add(newRequest);
@@ -28,6 +40,7 @@ public class DisastDowntime_260708548 {
                     serversList.add(new ArrayList<Integer>(serverRequestsPerSecond));
                     serversList.get(serversList.size()-1).add(newRequest);
                     addedRequest = true;
+                    break;
                 }
             }
         }
@@ -36,11 +49,36 @@ public class DisastDowntime_260708548 {
     }
 
 
-    public static void updateServer(ArrayList<Integer> server, int newRequest) {
-        for(int i = 0; i < server.size(); i++) {
+    public static void updateServer(List<Integer> server, int newRequest) {
+        /**
+         * more test cases solved
+         * with this than with removeIf()
+         */
+        // boolean requestProcessed = false;
+        // int i = 0;
+        // int last = server.size()-1;
+        // while(true) { //requestProcessed
+        //     if(newRequest - server.get(last-i) >= 1000) {
+        //         server = server.subList(last-i+1, server.size());
+        //         // requestProcessed = true;
+        //         break;
+        //     }
+        //     else {
+        //         i++;
+        //     }
+        // }
+        for(int i = server.size()-1; i > -1; i--) {
             if (newRequest - server.get(i) >= 1000) {
-                server.remove(i);
+                server = server.subList(i+1, server.size());
+                break;
             }
         }
     }
+
+
+    // public static int binarySearch(List<Integer> server, int newRequest) {
+    //     int position = server.size()/2;
+    //     int difference = newRequest - server.get(position);
+    //     if ()
+    // }
 }
