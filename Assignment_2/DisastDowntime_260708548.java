7b2103fb4b75f53b8f8f0677c355cebc00aae558
atg-abhijay <atg.abhijay@gmail.com>
2018-09-30 16:07:19 -0400

    working now. passing only 5/27 test cases. time limit exceeded

--- Assignment_2/DisastDowntime_260708548.java
+++ Assignment_2/DisastDowntime_260708548.java
@@ -9,76 +9,67 @@ public class DisastDowntime_260708548 {
         Scanner sc = new Scanner(System.in);
         int upcomingRequests = sc.nextInt();
         int serverRequestsPerSecond = sc.nextInt();
-        HashMap servers = new HashMap<Integer, List<Integer>>();
-        int num_servers = servers.size();
+        /**
+         * make a Hashmap to store all the servers
+         */
+        HashMap<Integer, List<Integer>> servers = new HashMap<>();
         List<Integer> serv = new ArrayList<Integer>();
+        /**
+         * initialize the hashmap with one empty server
+         */
         servers.put(0, serv);
-        // System.out.println(servers.get(0));
-        // List<List<Integer>> serversList = new ArrayList<List<Integer>>();
-        // List<Integer> server = new ArrayList<>();
-        // for(int i = 0; i < serversList.size(); i++) {
-        //     /**
-        //      * more time efficient to initialize the size of
-        //      * the arraylists than to not specify the size
-        //      */
-        //     serversList.add(new ArrayList<Integer>(serverRequestsPerSecond));
-        // }
         for(int i = 0; i < upcomingRequests; i++) {
             boolean addedRequest = false;
             int newRequest = sc.nextInt();
             while (!addedRequest) {
                 for (int j = 0; j < servers.size(); j++) {
-                    server = servers.get(j);
-                    updateServer(server, newRequest);
+                    List<Integer> server = servers.get(j);
+                    server = updateServer(server, newRequest);
                     if (server.size() != serverRequestsPerSecond) {
                         server.add(newRequest);
+                        // printList(server);
                         addedRequest = true;
+                        servers.put(j, server);
                         break;
                     }
                 }
                 if (!addedRequest) {
-                    serversList.add(new ArrayList<Integer>(serverRequestsPerSecond));
-                    serversList.get(serversList.size()-1).add(newRequest);
+                    List<Integer> new_serv = new ArrayList<Integer>();
+                    new_serv.add(newRequest);
+                    servers.put(servers.size(), new_serv);
                     addedRequest = true;
                     break;
                 }
             }
         }
         sc.close();
-        System.out.println(serversList.size());
+        System.out.println(servers.size());
     }
 
 
-    public static void updateServer(List<Integer> server, int newRequest) {
-        /**
-         * more test cases solved
-         * with this than with removeIf()
-         */
-        // boolean requestProcessed = false;
-        // int i = 0;
-        // int last = server.size()-1;
-        // while(true) { //requestProcessed
-        //     if(newRequest - server.get(last-i) >= 1000) {
-        //         server = server.subList(last-i+1, server.size());
-        //         // requestProcessed = true;
-        //         break;
-        //     }
-        //     else {
-        //         i++;
-        //     }
-        // }
-        for(int i = server.size()-1; i > -1; i--) {
+    public static List<Integer> updateServer(List<Integer> server, int newRequest) {
+        for(int i = server.size() - 1; i > -1; i--) {
             if (newRequest - server.get(i) >= 1000) {
-                server = server.subList(i+1, server.size());
-                break;
+                if (i == server.size() - 1) {
+                    server = new ArrayList<Integer>();
+                    return server;
+                }
+                else{
+                    server = server.subList(i+1, server.size());
+                    return server;
+                }
             }
         }
+
+        return server;
     }
 
 
-    // public static int binarySearch(List<Integer> server, int newRequest) {
-    //     int position = server.size()/2;
-    //     int difference = newRequest - server.get(position);
-    //     if ()
-    // }
+    public static void printList(List<Integer> list) {
+        System.out.print("List: ");
+        for(int num: list) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 }
