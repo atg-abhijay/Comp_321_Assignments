1dd21c27d16848bc3420a35e16d07a8b470b1ff5
atg-abhijay <atg.abhijay@gmail.com>
2018-09-30 22:56:46 -0400

    renamed DDTime

--- Assignment_2/DDTime_260708548.java
+++ Assignment_2/DDTime_260708548.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
-public class DDTime {
+public class DDTime_260708548 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int numRequests = sc.nextInt();
