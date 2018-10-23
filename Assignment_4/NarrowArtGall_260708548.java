2c9f0d9437a414243a2186b7b976eb7692e25720
atg-abhijay <atg.abhijay@gmail.com>
2018-10-22 23:51:39 -0400

    Ass 4 - NAG - added comments

--- Assignment_4/NarrowArtGall_260708548.java
+++ Assignment_4/NarrowArtGall_260708548.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class NarrowArtGall_260708548 {
     public static void main(String[] args) {
+        /**
+         * storing a constant "invalid"
+         * value for the initializations
+         * performed below.
+         */
         final Integer negativeConstant = Integer.MIN_VALUE;
         Scanner sc = new Scanner(System.in);
+        /**
+         * keep running the loop until all
+         * galleries have been evaluated.
+         */
         while(sc.hasNextInt()) {
             int numRows = sc.nextInt();
             int numCloseRooms = sc.nextInt();
@@ -13,6 +22,16 @@ public class NarrowArtGall_260708548 {
                 artGallery[p][1] = sc.nextInt();
             }
 
+            /**
+             * 3-dimensional array to
+             * keep track of everything.
+             * the third dimension has
+             * size three to store three
+             * types of outcomes:
+             * 1. closing left room
+             * 2. closing right room
+             * 3. closing neither room
+             */
             int[][][] table = new int[numRows][numCloseRooms+1][3];
             for(int p = 0; p < numRows; p++) {
                 for(int q = 0; q < numCloseRooms+1; q++) {
@@ -22,11 +41,18 @@ public class NarrowArtGall_260708548 {
                 }
             }
 
+            /**
+             * values for the base cases
+             */
             int[] row_1 = table[0][0];
             row_1[0] = negativeConstant;
             row_1[1] = negativeConstant;
             row_1[2] = artGallery[0][0] + artGallery[0][1];
 
+            /**
+             * making updates for the
+             * values along different rows.
+             */
             for(int p = 1; p < numRows; p++) {
                 int[] row_2 = table[p][0];
                 row_2[0] = negativeConstant;
@@ -41,6 +67,11 @@ public class NarrowArtGall_260708548 {
                 row_3[2] = negativeConstant;
             }
 
+            /**
+             * significant loop where entries
+             * in the table are updated using
+             * values of their "predecessors".
+             */
             for(int p = 1; p < numRows; p++) {
                 int bound = Math.min(p+1, numCloseRooms);
                 for(int q = 1; q < bound + 1; q++) {
@@ -59,9 +90,18 @@ public class NarrowArtGall_260708548 {
                 }
             }
 
+            /**
+             * go to the last row to find
+             * the maximum values and
+             * store them to return them.
+             */
             int[] row_7 = table[numRows-1][numCloseRooms];
             int max_5 = Math.max(row_7[1], row_7[2]);
             int result = Math.max(row_7[0], max_5);
+            /**
+             * skip over the last '0 0' which
+             * denotes the end of a gallery.
+             */
             sc.nextInt();
             sc.nextInt();
             System.out.println(result);
