d11d870a4f4882597a2513e7e9764ac9b0dcad67
atg-abhijay <atg.abhijay@gmail.com>
2018-10-28 18:11:02 -0400

    A5 - solution still not fast enough after removing copying of PQ

--- Assignment_5/ShortestPath_260708548.java
+++ Assignment_5/ShortestPath_260708548.java
@@ -26,32 +26,18 @@ class Edge {
     public int t0;
     public int P;
     public int traverseTime;
-    // public int weight;
 
     Edge(Vertex v, int t0, int p, int timeToTraverse) {
         this.endVertex = v;
         this.t0 = t0;
         this.P = p;
         this.traverseTime = timeToTraverse;
-        // this.weight = w;
     }
 }
 
 
 public class ShortestPath_260708548 {
-    // static int time = 0;
     public static void main(String[] args) {
-        // PriorityQueue<Integer> pq = new PriorityQueue<>();
-        // int[] sample = new int[] {53, 72, 19, 24, 35, 43};
-        // for(int num: sample) {
-        //     pq.add(num);
-        // }
-        // sample[0] = 3;
-        // sample[2] = 12;
-        // while(!pq.isEmpty()) {
-        //     System.out.println(pq.poll());
-        // }
-        // System.out.println("Welcome!");
         Scanner sc = new Scanner(System.in);
         while(true) {
             /**
@@ -61,7 +47,6 @@ public class ShortestPath_260708548 {
             int numEdges = sc.nextInt();
             int numQueries = sc.nextInt();
             int sourceVertex = sc.nextInt();
-            // System.out.println(numNodes + ", " + numEdges + ", " + numQueries + ", " + sourceVertex);
 
             /**
              * Finished all the test cases
@@ -77,10 +62,6 @@ public class ShortestPath_260708548 {
             Vertex[] vertices = new Vertex[numNodes];
             Vertex[] predecessors = new Vertex[numNodes];
             init(vertices, predecessors, numNodes, sourceVertex);
-            // for(int i = 0; i < vertices.length; i++) {
-            //     Vertex v = vertices[i];
-            //     System.out.println("Index: " + i + ", " + v.shortestDistance);
-            // }
 
             /**
              * verticesDetermined - vertices whose final shortest-path
@@ -98,7 +79,6 @@ public class ShortestPath_260708548 {
             for(int i = 0; i < numNodes; i++) {
                 pq.add(vertices[i]);
             }
-            // System.out.println("Added vertices to Priority Queue.");
 
             /**
              * adding edges connected to the vertices
@@ -109,7 +89,6 @@ public class ShortestPath_260708548 {
                                   sc.nextInt(), sc.nextInt());
                 startVertex.addEdge(e);
             }
-            // System.out.println("Finished making and adding edges.");
 
             while(!pq.isEmpty()) {
                 Vertex u = pq.peek();
@@ -120,9 +99,7 @@ public class ShortestPath_260708548 {
                     }
                 }
                 pq.poll();
-                Vertex[] pqArray = new Vertex[pq.size()];
-                pqArray = pq.toArray(pqArray);
-                pq = convertArrayToPQ(pqArray);
+                // pq = new PriorityQueue<Vertex>(pq);
             }
 
             for(int i = 0; i < numQueries; i++) {
@@ -131,22 +108,11 @@ public class ShortestPath_260708548 {
                     System.out.println("Impossible");
                 }
                 else {
-                    // System.out.println("Vertex #: " + v.number + ", Distance: " + v.shortestDistance);
                     System.out.println(queryNode.shortestDistance);
                 }
 
             }
             System.out.println();
-
-            // for(Vertex v: verticesDetermined) {
-            //     if(v.shortestDistance == Integer.MAX_VALUE) {
-            //         System.out.println("Impossible");
-            //     }
-            //     else {
-            //         // System.out.println("Vertex #: " + v.number + ", Distance: " + v.shortestDistance);
-            //         System.out.println(v.shortestDistance);
-            //     }
-            // }
         }
     }
 
@@ -184,27 +150,10 @@ public class ShortestPath_260708548 {
                 int newTime = startVertex.shortestDistance + timeWaited + e.traverseTime;
                 if(v.shortestDistance > newTime) {
                     v.shortestDistance = newTime;
-                    // System.out.println(startVertex.number + ", " + v.number + ", " + v.shortestDistance);
                     predecessors[v.number] = startVertex;
                     break;
                 }
             }
         }
-    }
-
-
-    public static PriorityQueue<Vertex> convertArrayToPQ(Vertex[] midExecutionVertices) {
-        PriorityQueue<Vertex> pq = new PriorityQueue<Vertex>(new Comparator<Vertex>() {
-            @Override
-            public int compare(Vertex v1, Vertex v2) {
-                return Integer.compare(v1.shortestDistance, v2.shortestDistance);
-            }
-        });
-
-        for(int i = 0; i < midExecutionVertices.length; i++) {
-            pq.add(midExecutionVertices[i]);
-        }
-
-        return pq;
     }
 }
