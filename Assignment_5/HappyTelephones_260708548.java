b464c5d9d48648d6f06a91b4893f896155c6eb41
atg-abhijay <atg.abhijay@gmail.com>
2018-10-28 23:37:47 -0400

    A5 - added comments for HT

--- Assignment_5/HappyTelephones_260708548.java
+++ Assignment_5/HappyTelephones_260708548.java
@@ -6,12 +6,24 @@ public class HappyTelephones_260708548 {
         while(true) {
             int numCalls = sc.nextInt();
             int numIntervals = sc.nextInt();
+            /**
+             * Marks end of test cases
+             */
             if(numCalls == 0 && numIntervals == 0) {
                 break;
             }
+            /**
+             * maintain two arrays to keep track
+             * of the start and end of calls
+             */
             int[] startOfCalls = new int[numCalls];
             int[] endOfCalls = new int[numCalls];
             for(int i = 0; i < numCalls; i++) {
+                /**
+                 * the source and destination
+                 * of the calls are never used,
+                 * so we can disregard them.
+                 */
                 sc.nextInt();
                 sc.nextInt();
                 int callStart = sc.nextInt();
@@ -23,6 +35,14 @@ public class HappyTelephones_260708548 {
                 int intervalEnd = intervalStart + sc.nextInt();
                 int numActive = 0;
                 for(int j = 0; j < numCalls; j++) {
+                    /**
+                     * if the call ends before the interval starts
+                     * or the call starts after the interval ends,
+                     * then we have nothing to do.
+                     *
+                     * if this is not the case, then that call is
+                     * active in the interval we are interested in.
+                     */
                     if(endOfCalls[j] <= intervalStart || startOfCalls[j] >= intervalEnd) {
                         // do nothing
                     }
