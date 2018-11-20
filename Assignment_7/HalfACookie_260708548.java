b107f90efe470cfaeb63b600d46e0dc663da7a07
atg-abhijay <atg.abhijay@gmail.com>
2018-11-20 01:05:18 -0500

    A7 - added comments

--- Assignment_7/HalfACookie_260708548.java
+++ Assignment_7/HalfACookie_260708548.java
@@ -8,19 +8,33 @@ public class HalfACookie_260708548 {
             double xStrike = sc.nextDouble();
             double yStrike = sc.nextDouble();
             double distanceFromOrigin = Math.sqrt(xStrike*xStrike + yStrike*yStrike);
+            /**
+             * strike point is
+             * outside the circle
+             */
             if(distanceFromOrigin > radius) {
                 System.out.println("miss");
+                /**
+                 * skip over the rest of the
+                 * code for this iteration
+                 */
                 continue;
             }
 
             final double pi = Math.PI;
             double totalArea = pi * radius * radius;
 
+            /**
+             * the angle formed by the radius and
+             * the chord at the boundary of the circle
+             */
             double angleAtChord = Math.asin(distanceFromOrigin/radius);
-            // System.out.println(angleAtChord);
             double angleAtCentre = pi - 2*angleAtChord;
+            /**
+             * chordLength = 2*(base of half-triangle)
+             * base of half-triangle = sqrt(radius^2 - distanceFromOrigin^2)
+             */
             double chordLength = 2*(Math.sqrt(Math.pow(radius, 2) - Math.pow(distanceFromOrigin, 2)));
-
 
             double areaOfSector = (angleAtCentre/(2*pi)) * totalArea;
             double areaOfTriangle = 0.5 * chordLength * distanceFromOrigin;
