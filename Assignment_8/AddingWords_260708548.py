0cdf700b74052cec7116e2df27c7d2ea9dd3168b
atg-abhijay <atg.abhijay@gmail.com>
2018-11-29 20:04:49 -0500

    A8 - added comments; simplified code a bit

--- Assignment_8/AddingWords_260708548.py
+++ Assignment_8/AddingWords_260708548.py
@@ -1,17 +1,24 @@
 from sys import stdin
 
 def main():
+    # dictionary with key = variable name
+    # and value = variable value
     vars_dict = {}
     for line in stdin.readlines():
         tokens = line.split(' ')
         first_token = tokens[0]
 
+        # check the first token to
+        # see if the command is 'def',
+        # 'calc' or 'clear'
         if first_token == 'def':
             vars_dict[tokens[1]] = int(tokens[2])
 
         elif first_token == 'calc':
             is_valid = True
             result, add_or_subtract, count = 0, 1, 1
+            # counts the number of terms/tokens
+            # except the 'calc' token and the '=' token
             num_terms = len(tokens) - 2
             while count <= num_terms:
                 token = tokens[count]
@@ -22,6 +29,9 @@ def main():
                     add_or_subtract = -1
 
                 else:
+                    # if the token has not been seen before,
+                    # the result of the expression is unknown
+                    # and we break
                     if token not in vars_dict:
                         print_answer(tokens, "unknown")
                         is_valid = False
@@ -31,6 +41,8 @@ def main():
 
                 count += 1
 
+            # at the end of the 'calc' expression,
+            # we check if the result itself is valid or not
             if is_valid:
                 check_result_exists(result, vars_dict, tokens)
 
@@ -39,19 +51,34 @@ def main():
 
 
 def print_answer(tokens, answer):
-    print(' '.join(tokens[1:-1]), '=', answer)
+    """
+    utility method to make
+    printing easier
+
+    :param tokens: list of tokens
+    :param answer: a string
+
+    """
+    print(' '.join(tokens[1:]), answer)
 
 
 def check_result_exists(result, vars_dict, tokens):
-    found = False
+    """
+    checks if the final result obtained
+    from the 'calc' expression exists
+    in the dictionary of variables or not
+
+    :param result: int
+    :param vars_dict: dict with (key, value) = (var_name, var_value)
+    :param tokens: list of tokens
+
+    """
     for var_name, val in vars_dict.items():
         if val == result:
             print_answer(tokens, var_name)
-            found = True
-            break
+            return
 
-    if not found:
-        print_answer(tokens, "unknown")
+    print_answer(tokens, "unknown")
 
 
 main()
